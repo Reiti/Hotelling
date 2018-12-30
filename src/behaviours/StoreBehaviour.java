effc85cd76de4ed61f6256bde83b107a59cafef3
Michael Pucher <michael.pucher.main@gmail.com>
2018-12-30 23:10:45 +0100

    Add large neighbourhood for 6 stores

--- src/behaviours/StoreBehaviour.java
+++ src/behaviours/StoreBehaviour.java
@@ -57,6 +57,9 @@ public class StoreBehaviour extends CyclicBehaviour {
             case "Step1":
                 directions = new int[]{-1, 1};
                 break;
+            case "Step20":
+                directions = new int[]{-20, -19, -18, -17, -16, -15, -14, -13, -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+                break;
             case "Step2":
             default:
                 directions = new int[]{-2, -1, 1, 2};
